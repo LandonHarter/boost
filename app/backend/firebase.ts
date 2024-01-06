6bf3aef4a734a86e528ee4c015260d05366d81d3
LandonHarter <landonharter@outlook.com>
2024-01-06 12:35:38 -0500

    chore: firebase setup

---
+++ app/backend/firebase.ts
@@ -0,0 +1,13 @@
+import { FirebaseOptions, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig: FirebaseOptions = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREABSE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+};
+export const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
